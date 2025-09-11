a139a6c3228ac3a3a3d608de33e88d842245145d
Your Name <marcosralbrecht@gmail.com>
2025-09-10 23:07:42 -0300

    feat: first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textColor}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#222222FF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  textColor: {
+    color: "#E4E4E4FF",
   },
 });
